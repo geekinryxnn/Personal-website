150420cfd9d779a32cbeba0c3ad78de8379886ea
geekinryxnn <ryankiratu67@gmail.com>
2025-02-28 17:20:01 +0300

    new changes

---
+++ Scripts.js/index.js
@@ -0,0 +1,6 @@
+
+function displayGreeting() {
+    const greetingMessage = "Hello, welcome to my personal website!";
+    console.log(greetingMessage);
+displayGreeting();}
+console.log(greetingMessage); // This will throw an error because greetingMessage is not defined in the global scope
